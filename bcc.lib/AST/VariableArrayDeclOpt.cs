2f57932842394ec9ba27182f725a32f46851368f
bartq <bci@poczta.onet.pl>
2016-02-24 19:36:57 +0100

    somehow missed adding that

---
+++ bcc.lib/AST/VariableArrayDeclOpt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bcc.lib.AST
+{
+    public class VariableArrayDeclOpt : Node
+    {
+        public bool Present { get; set; }
+        public override void StepOut(IContext context)
+        {
+            this.Present = this.ParseNode.ChildNodes.Count > 0;
+            base.StepOut(context);
+        }
+    }
+}
